55431ad0ef5bfbc1d5b81a9bdeaa86bd90afcea5
Diego <diego.pereirasilva@live.com>
2016-09-04 21:16:27 -0300

    new

--- index.js
+++ index.js
@@ -26,6 +26,40 @@ app.get('/webhook/', function (req, res) {
     res.send('Error, wrong token')
 })
 
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Mensagem recebida, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAIM2gyEGTYBAJmr1302WOujRbB2TmcnbSzK1Q8RwG37mWp9LlEYwIlZCV0buoP7bnso8kJRiKWoAZBRvWk0kZCh188QNPRzQg50UjfJ1CXAMtG8uXRGHSERLFAMWUC53JwIxmagiZAmH9cWD67yL3a2QBA8l51SIvaxydOYIQZDZD"
+
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
