d471ebab500ab7dd932f294124e192c4aa7167a2
Diego <diego.pereirasilva@live.com>
2016-09-04 21:40:08 -0300

    funfou

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/webhook/', function (req, res) {
                 sendGenericMessage(sender)
                 continue
             }
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            sendTextMessage(sender, "Mensagem recebida, Isso: " + text.substring(0, 200) + " ?")
         }
     }
     res.sendStatus(200)
